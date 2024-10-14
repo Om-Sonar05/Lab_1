5ea50c03be40ddfeeb67035dfefb40e618e2fbde
Om Sonar <2023ucp1831@mnit.ac.in>
2024-10-14 15:13:41 +0530

    another commit

--- simplepython.py
+++ simplepython.py
@@ -6,7 +6,7 @@ def dot_product(vector1, vector2):
     return sum(products)
 
 # Example usage:
-v1 = [1, 2, 3]
+v1 = [1, 2, 10]
 v2 = [4, 5, 6]
 print(f"The dot product is: {dot_product(v1, v2)}")
 
