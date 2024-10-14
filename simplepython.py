5c2d3e822c427603d9fe4bb9c8a1d8c53943736e
Om Sonar <2023ucp1831@mnit.ac.in>
2024-10-14 14:46:42 +0530

    Added parallelism

--- simplepython.py
+++ simplepython.py
@@ -1,6 +1,9 @@
-# dot_product.py
+import concurrent.futures
+
 def dot_product(vector1, vector2):
-    return sum(x * y for x, y in zip(vector1, vector2))
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        products = list(executor.map(lambda x: x[0] * x[1], zip(vector1, vector2)))
+    return sum(products)
 
 # Example usage:
 v1 = [1, 2, 3]
