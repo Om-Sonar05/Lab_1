8c930853c4d6d33f5947259770bf38ec225cfb08
Om Sonar <2023ucp1831@mnit.ac.in>
2024-10-14 15:01:03 +0530

    Changes made

--- simplepython.py
+++ simplepython.py
@@ -5,5 +5,5 @@ def dot_product(vector1, vector2):
 # Example usage:
 v1 = [1, 2, 6]
 v2 = [4, 5, 6]
-print(f"The dot product is: {dot_product(v1, v2)}")
+print(f"The dot product of the vectors is: {dot_product(v1, v2)}")
 
